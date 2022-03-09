30ad81467b9b80db524d35e48890dfb924f53539
malinjr07 <malinjr07@gmail.com>
2022-03-09 17:57:45 +0600

    testimonials done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import About from './components/About';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Pricing />
+      <Testimonials />
     </>
   );
 }
